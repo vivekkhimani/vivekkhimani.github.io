c44fce899aeefc15bc3e2f672fa2cf8b0daf7a2b
vivekkhimani <vivekkhimani07@gmail.com>
2021-12-27 01:22:10 -0500

    revert

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  // $receiving_email_address = 'vivekkhimani07@gmail.com';
+  $receiving_email_address = 'vivekkhimani07@gmail.com';
 
   // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
   //   include( $php_email_form );
@@ -18,11 +18,11 @@
   //$contact = new PHP_Email_Form;
   //$contact->ajax = true;
   
-  // $to = $receiving_email_address;
-  // $from_name = $_POST['name'];
-  // $from_email = $_POST['email'];
-  // $subject = $_POST['subject'];
-  // $message = $_POST['message'];
+  $to = $receiving_email_address;
+  $from_name = $_POST['name'];
+  $from_email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -34,15 +34,16 @@
   );
   */
 
-  // $body = "From: $from_name\n E-Mail: $from_email\n Subject:\n $subject\n Message:\n $message";
+  $body = "From: $from_name\n E-Mail: $from_email\n Subject:\n $subject\n Message:\n $message";
 
-  // if ($_POST['submit']) {
-  //   if (mail ($to, $subject, $body, $from)) {
-  //     echo '<p>Your message has been sent!</p>';
-  //   } else {
-  //     echo '<p>Something went wrong! Try again!</p>';
-  //   }
-  // }
+  if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) {
+      echo '<p>Your message has been sent!</p>';
+    } else {
+      echo '<p>Something went wrong! Try again!</p>';
+    }
+  }
 
   //echo $contact->send();
+
 ?>
